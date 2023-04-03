a7a55aa94b9870f98f4a52fcd8412f8dc19576d1
Daniel Iglesias <dev.daif@gmail.com>
2023-04-03 03:25:08 -0400

    fix: deleting styles for page, not using them.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,8 @@
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
-
-import cn from "classnames"
 import CreateRoom from '@/app/rooms/create'
 
 export default function Home() {
   return (
-    <main className={cn(styles.main, inter.className)}>
+    <main>
       <CreateRoom/>
     </main>
   )
