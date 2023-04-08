347803eab177832dc177cfc71e6e5d55dbe70649
Daniel Iglesias <dev.daif@gmail.com>
2023-04-08 05:59:40 -0400

    feat: now firebase-admin use env vars for connection

--- src/app/firebase/admin.ts
+++ src/app/firebase/admin.ts
@@ -1,11 +1,19 @@
-import { initializeApp, applicationDefault, getApps} from "firebase-admin/app";
+import { initializeApp, getApps, cert} from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore"
 import { getAuth } from "firebase-admin/auth";
 
+const privateKey = process.env.FIREBASE_CREDENTIAL_PRIVATE_KEY?.replace(/\\n/g,"\n");
 
-getApps()
 const app = (!getApps().length) ? initializeApp({
-  credential: applicationDefault()
+  credential: cert({
+    projectId:process.env.FIREBASE_CREDENTIAL_PROJECT_ID,
+    clientEmail:process.env.FIREBASE_CREDENTIAL_CLIENT_EMAIL,
+    privateKey
+  })
 }) : getApps()[0];
+
+//const app = (!getApps().length) ? initializeApp({
+//credential: applicationDefault()
+//}) : getApps()[0];
 export const auth = getAuth();
 export const db = getFirestore(app);
