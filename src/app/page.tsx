57f0db396f4014719b57da060d61c9dcac495929
Daniel Iglesias <dev.daif@gmail.com>
2023-07-14 10:47:03 -0400

    feat: added header to home page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,22 @@ import styles from "@/app/page.module.css";
 
 import GoToRooms from "@/app/rooms/components/GoToRooms";
 
+import { Monoton } from "next/font/google";
+
+const monoton = Monoton({
+  weight: "400",
+  subsets: ["latin"],
+})
+
 export default function Home() {
   return (
-    <main className={styles.center}>
-      <GoToRooms />
+    <main className={`${styles.center} flex flex-col gap-4 max-w-2xl m-auto`}>
+      <header className="flex flex-col gap-2 items-center">
+        <h1 className={`${monoton.className} text-5xl text-center leading-tight text-black dark:text-white`}>
+          Instant fun with exciting mini games.
+        </h1>
+        <GoToRooms />
+      </header>
     </main>
   );
 }
