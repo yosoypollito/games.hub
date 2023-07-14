d5502bbbbc56a5fc12716e44dee08d1a58926b62
Daniel Iglesias <dev.daif@gmail.com>
2023-07-14 10:40:24 -0400

    feat: added dark mode to navbar new styles

--- src/app/nav.bar.tsx
+++ src/app/nav.bar.tsx
@@ -8,7 +8,7 @@ export default function NavBar() {
     <nav className={styles.nav}>
 
       <ThemeButton />
-      <div className="flex gap-4 [&>a]:text-red [&>a]:font-semibold [&>a:hover]:text-white">
+      <div className="flex gap-4 [&>a:hover]:text-light-blue dark:[&>a]:text-red dark:[&>a]:font-semibold dark:[&>a:hover]:text-white">
         <Link href="/">Home</Link>
         <Link href="/rooms">Rooms</Link>
         <a href="https://www.buymeacoffee.com/daif" target="_blank" rel="noreferrer">Contribute</a>
