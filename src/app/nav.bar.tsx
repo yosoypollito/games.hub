31ef87efd06e883d81477620ffd23f05946e6a91
Daniel Iglesias <dev.daif@gmail.com>
2023-07-14 10:28:33 -0400

    feat: added more links to navbar and few style changes.

--- src/app/nav.bar.tsx
+++ src/app/nav.bar.tsx
@@ -6,8 +6,13 @@ import styles from "@/app/nav.bar.module.css";
 export default function NavBar() {
   return (
     <nav className={styles.nav}>
-      <Link href="/">Home</Link>
+
       <ThemeButton />
+      <div className="flex gap-4 [&>a]:text-red [&>a]:font-semibold [&>a:hover]:text-white">
+        <Link href="/">Home</Link>
+        <Link href="/rooms">Rooms</Link>
+        <a href="https://www.buymeacoffee.com/daif" target="_blank" rel="noreferrer">Contribute</a>
+      </div>
     </nav>
   );
 }
