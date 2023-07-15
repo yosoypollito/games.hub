9d21644ebd167f359f6680d2bff772bd4a7b8185
Daniel Iglesias <dev.daif@gmail.com>
2023-07-14 21:56:58 -0400

    style: gap changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { monoton } from "@/constants";
 export default function Home() {
   return (
     <main className={`${styles.center} flex flex-col gap-4m-auto`}>
-      <header className="flex flex-col gap-2 items-center">
+      <header className="flex flex-col gap-4 items-center">
         <h1 className={`${monoton.className} text-5xl text-center leading-tight text-black dark:text-white`}>
           Instant fun with exciting mini games.
         </h1>
