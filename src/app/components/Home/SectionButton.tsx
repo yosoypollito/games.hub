33c1409b6db81137b643f4eb9dbf9760e84758bc
Daniel Iglesias <dev.daif@gmail.com>
2023-07-20 04:36:54 -0400

    feat: Section button props now are handled correctly

--- src/app/components/Home/SectionButton.tsx
+++ src/app/components/Home/SectionButton.tsx
@@ -1,4 +1,6 @@
-export default function SectionButton({ props, children }: { props?: React.ComponentProps<"button">; children: React.ReactNode }) {
+import { FC } from "react";
+
+const SectionButton: FC<React.ButtonHTMLAttributes<HTMLButtonElement>> = ({ children, ...props }) => {
   return (
     <button
       className="px-4 py-2 bg-light-blue text-black font-semibold rounded-lg shadow-sm hover:scale-105 transition-all dark:text-white dark:bg-red"
@@ -7,3 +9,5 @@ export default function SectionButton({ props, children }: { props?: React.Compo
     </button>
   )
 }
+
+export default SectionButton
