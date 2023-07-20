a81181709415592e6092ebaed6e7d4ef1de1e262
Daniel Iglesias <dev.daif@gmail.com>
2023-07-20 05:15:07 -0400

    feat: leave game idea front finish

--- src/app/components/Home/LeaveYourIdeaSection.tsx
+++ src/app/components/Home/LeaveYourIdeaSection.tsx
@@ -5,13 +5,14 @@ import SectionButton from "./SectionButton";
 
 import Modal from "@/components/Modal";
 import { useState } from "react";
-import { Formik, Form, Field, FormikProps, FieldProps } from "formik";
+import { Formik, Form, Field, FormikProps } from "formik";
 import Label from "../Label";
 import FormGroup from "../FormGroup";
+import FormikInput from "../FormikInput";
 
-const Input = ({ field, form, ...props }: FieldProps) => {
-  return <input className="flex w-full border-b-2 border-light-blue dark:border-red p-2 py-1 bg-black/5 shadow-sm min-w-[248px]" {...field} {...props} />;
-};
+import { toast, Toaster } from "react-hot-toast";
+
+
 
 export default function LeaveYourIdeaSection() {
 
@@ -32,7 +33,9 @@ export default function LeaveYourIdeaSection() {
             howToPlay: ""
           }}
           onSubmit={() => {
-            console.log("Hello")
+            toast("We are currently working in this feature, come back later!", {
+              icon: "🚀",
+            })
           }}
         >
           {(props: FormikProps<any>) => (
@@ -40,21 +43,22 @@ export default function LeaveYourIdeaSection() {
               <div className="flex gap-2">
                 <FormGroup>
                   <Label htmlFor="email">Email</Label>
-                  <Field type="email" name="email" component={Input} />
+                  <Field type="email" name="email" component={FormikInput} />
                 </FormGroup>
                 <FormGroup>
                   <Label htmlFor="game_name">Game name</Label>
-                  <Field name="game_name" component={Input} />
+                  <Field name="game_name" component={FormikInput} />
                 </FormGroup>
               </div>
               <FormGroup>
                 <Label htmlFor="how_to_play">How to play</Label>
-                <Field name="how_to_play" component={Input} />
+                <Field name="how_to_play" component={FormikInput} />
               </FormGroup>
               <SectionButton type="submit">Let&apos;s make it happen!</SectionButton>
             </Form>
           )}
         </Formik>
+        <Toaster position="bottom-right" />
       </Modal>
     </HomeSection>
   )
